ed227b63c2a6adf94fb8618c32ce9af2c68d0340
Kevin Gilpin <kgilpin@gmail.com>
2014-01-02 16:40:09 -0500

    be selective about forwarded headers, and test that it works with conjurops account

--- lib/conjur/webserver/api_proxy.rb
+++ lib/conjur/webserver/api_proxy.rb
@@ -3,19 +3,29 @@ module Conjur
     require 'rack/proxy'
     
     class APIProxy < Rack::Proxy
-      def rewrite_env(env)
-        env["HTTP_X_FORWARDED_SSL"] = "on"
-        path = env["PATH_INFO"]
-        if path =~ /^\/api\/([^\/]+)\//
+      def rewrite_env(source_env)
+        path = source_env["PATH_INFO"]
+
+        service_url = if path =~ /^\/api\/([^\/]+)\//
           app = $1
-          env["HTTP_X_FORWARDED_HOST"] = "#{app}-ci-conjur.herokuapp.com"
+          Conjur.configuration.send("#{app}_url")
         else
-          env["HTTP_X_FORWARDED_HOST"] = "core-ci-conjur.herokuapp.com"
+          Conjur.configuration.send("core_url")
         end
-        env["HTTP_AUTHORIZATION"] = authorization_header
-        env["PATH_INFO"] = path.gsub(/^\/api\//, "/")
-        env.delete "HTTP_HOST"
+        service_url = URI.parse(service_url)
+        host = service_url.hostname
+        script = service_url.path
         
+        env = source_env.with_indifferent_access.slice(*%w(REQUEST_METHOD QUERY_STRING REMOTE_ADDR REMOTE_HOST HTTP_CONNECTION HTTP_ACCEPT HTTP_X_REQUESTED_WITH HTTP_USER_AGENT HTTP_REFERER HTTP_ACCEPT_ENCODING HTTP_ACCEPT_LANGUAGE HTTP_VERSION))
+        
+        env.merge!({
+          "HTTP_X_FORWARDED_SSL" => "on",
+          "HTTP_X_FORWARDED_HOST" => host,
+          "SCRIPT_NAME" => script,
+          "PATH_INFO" => path.gsub(/^\/api\//, "/"),
+          "HTTP_AUTHORIZATION" => authorization_header
+        })
+
         env
       end
     
