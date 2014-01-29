8077f5aaf41bdbfe5216912f281be9027dc63af5
Rafał Rzepecki <divided.mind@gmail.com>
2014-01-29 11:38:16 +0000

    Group members loaded.

--- public/js/groups.jsx
+++ public/js/groups.jsx
@@ -1,26 +1,58 @@
 /** @jsx React.DOM */
 
+var Group = React.createClass({
+  getInitialState: function() {
+    return { members: [] };
+  },
+
+  render: function() {
+    console.log(this.state);
+    return (
+      <li key={this.props.id}>
+        <h2>{this.props.id}</h2>
+        <ul>{
+          this.state.members.map(function(member) {
+            return <li>{member}</li>;
+          })
+        }</ul>
+      </li>
+    );
+  },
+
+  componentWillMount: function() {
+    var account = this.props.roleid.split(":")[0];
+
+    $.get("/api/authz/" + account + "/roles/group/" + encodeURIComponent(this.props.id) + "/?members",
+      function(members) {
+        console.log(arguments);
+        this.setState({
+          members: members.map(function(data) { return data.member; })
+        });
+      }.bind(this)
+    );
+  }
+});
+
 var Groups = React.createClass({
   getInitialState: function() {
     return { groups: [] };
   },
 
   render: function() {
+    var groupNodes = this.state.groups.map(function(group) {
+      group.key = group.id;
+      return Group(group);
+    });
     return (
       <ul>
-        {
-          this.state.groups.map(function(group) {
-            return (
-              <li key={group.id}>{group.id}</li>
-            )
-          })
-        }
+        {groupNodes}
       </ul>
     );
   },
 
   componentWillMount: function() {
     $.get("/api/groups", function(data) {
+        console.log(data);
         this.setState({groups: data});
       }.bind(this)
     );
